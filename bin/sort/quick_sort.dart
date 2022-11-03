4286cb7c063cd65248552942f1f73bb41ec398af
Sinan <sinanac124@gmail.com>
2022-11-03 11:57:17 +0530

    quick sort completed

---
+++ bin/sort/quick_sort.dart
@@ -0,0 +1,44 @@
+void main() {
+  List<int> arr = [30, 2, 41, 56, 12, 11, 22, 41];
+  List<int> result = QuickSort.quickSort(arr);
+  print(result);
+}
+
+class QuickSort {
+  static List<int> quickSort(List<int> arr) {
+    quickSortHelper(arr, 0, arr.length-1);    
+    return arr;
+  }
+
+  static void quickSortHelper(List<int> arr, int startIdx, int endIdx) {
+    if (startIdx >= endIdx) {
+      return;
+    }
+    int pivotIdx = startIdx;
+    int leftIdx = startIdx + 1;
+    int rightIdx = endIdx;
+
+    while (leftIdx <= rightIdx) {
+      if (arr[leftIdx] > arr[pivotIdx] && arr[rightIdx] < arr[pivotIdx]) {
+        swap(arr, leftIdx, rightIdx);
+        leftIdx++;
+        rightIdx--;
+      }
+      if (arr[leftIdx] <= arr[pivotIdx]) {
+        leftIdx++;
+      }
+      if (arr[rightIdx] >= arr[pivotIdx]) {
+        rightIdx--;
+      }
+    }
+    swap(arr, rightIdx, pivotIdx);
+    quickSortHelper(arr, startIdx, rightIdx-1);
+    quickSortHelper(arr, rightIdx+1, endIdx);
+  }
+
+  static void swap(List<int> arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
